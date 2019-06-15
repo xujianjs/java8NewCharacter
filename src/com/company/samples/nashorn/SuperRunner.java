beaf938e84d9fbd72d5340a05dbfa2c675fa0c4a
xujian <690378622@qq.com>
2019-06-15 22:34:41 +0800

    java8特性

---
+++ src/com/company/samples/nashorn/SuperRunner.java
@@ -0,0 +1,13 @@
+package com.company.samples.nashorn;
+
+/**
+ * @author Benjamin Winterberg
+ */
+public class SuperRunner implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("super run");
+    }
+
+}
