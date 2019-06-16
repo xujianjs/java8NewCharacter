c2def7ba98b888d794b719710cc7f4f05383e0f4
xujian <690378622@qq.com>
2019-06-16 09:36:59 +0800

    字节缓冲

---
+++ src/com/company/samples/misc/ByteBuffer1.java
@@ -0,0 +1,17 @@
+package com.company.samples.misc;
+
+
+/**
+ * xujian
+ */
+public class ByteBuffer1 {
+
+    public static void main(String[] args) {
+//        ByteBuffer类位于java.nio包下，所谓nio:代表new io,另一种解释：N代表Non-blocking IO,非阻塞的IO
+//        以下表格简要说明了NIO和IO的区别，接着我们将详细说明表格中的每个不同点。
+//          IO	                       NIO
+//          流式（Stream oriented）	缓冲式（Buffer oriented）
+//          阻塞IO	                非阻塞IO
+// 	                                选择器（Selectors）
+    }
+}
